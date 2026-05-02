60a26ec5250ce6bdae42a010cb6adb6a986a6faa
love-developer <lovekumaramrani13@gmail.com>
2026-05-02 18:48:41 +0500

    Navbar completed

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,17 +3,18 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='w-full'>
-            <div className='w-full max-w-[1336px] mx-auto h-[80px] flex items-center '>
-                <div className='w-[711px] h-[58px] flex gap-[16px] flex items-center '>
+            <div className='w-full max-w-[1336px] mx-auto min-h-[90px] flex items-center p-2'>
+                <div className='w-full max-w-[711px] min-h-[58px] flex gap-[16px] flex items-center '>
                     <div className='w-[61px] h-[56px]'>
                         <img src="/assets/images/logo.png" className='w-full h-full object-conver' alt="" />
                     </div>
                     <div className=''>
-                        <h1 className='text-[#111111] text-[32px] font-bold'>Seekheed UniverCity Enrollment Syatem</h1>
+                        <h1 className='text-[#111111] text-[18px] sm:text-[24px] md:text-[32px] font-bold leading-tight'>Seekheed UniverCity Enrollment Syatem</h1>
                         <p className='text-[#7C8091] text-[14px]'>University Enrollment Path</p>
                     </div>
                 </div>
             </div>
+            <div className='w-full h-[8px] bg-[#FF9600]'></div>
         </div>
     )
 }
